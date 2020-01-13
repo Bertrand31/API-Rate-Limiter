5767441f62a8299527c955df74082b32e7beaf49
Bertrand31 <bertrandjun@gmail.com>
2020-01-13 14:20:03 +0100

    Improve RateLimiter APIs + add documentation

--- src/main/scala/com/agoda/ratelimiting/RateLimiter.scala
+++ src/main/scala/com/agoda/ratelimiting/RateLimiter.scala
@@ -1,14 +1,19 @@
 package com.agoda.ratelimiting
 
 import scala.collection.mutable.Queue
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration._
 
 object RateLimiter {
 
-  def wrap[A, B](fn: A => B, seconds: Int, maxCalls: Int): A => Option[B] = {
+  /** Wraps a unary function and changes its signature so that it returns None when the rate limit
+    * has been met (thus not running the function), and Some(the output of the function) otherwise.
+    */
+  def wrapUnary[A, B](fn: A => B, cooldown: Duration = 10.seconds, maxCalls: Int = 50): A => Option[B] = {
     val visits = Queue[Long]()
     (arg: A) => {
       val now = System.currentTimeMillis
-      visits.dequeueAll(_ < (now - (seconds * 1000)))
+      visits.dequeueAll(_ < (now - cooldown.toMillis))
       if (visits.length >= maxCalls) None
       else {
         visits += now
