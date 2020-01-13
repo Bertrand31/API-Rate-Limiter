cfe97c7f136d6eec4ef6f6b7659aea1d23675ebd
Bertrand31 <bertrandjun@gmail.com>
2020-01-13 21:27:22 +0100

    Move controller logic to Controller file

--- src/main/scala/com/agoda/ratelimiting/HotelsController.scala
+++ src/main/scala/com/agoda/ratelimiting/HotelsController.scala
@@ -2,9 +2,23 @@ package com.agoda.ratelimiting
 
 import scala.concurrent.duration.DurationInt
 import cats.effect.IO
+import cats.implicits._
+import org.http4s.Response
+import org.http4s.dsl.io._
+import io.circe.syntax.EncoderOps
+import io.circe.generic.auto.exportEncoder
+import org.http4s.circe.CirceEntityEncoder.circeEntityEncoder
 import com.agoda.ratelimiting.types.Hotel
 
 object HotelsController {
+
+  private val handleSuccess: IO[Array[Hotel]] => IO[Response[IO]] =
+    _ >>= ((arr: Array[Hotel]) => Ok(arr.asJson))
+
+  private val handleLimited: IO[Response[IO]] = TooManyRequests("Too many requests")
+
+  private val handleReponse: Option[IO[Array[Hotel]]] => IO[Response[IO]] =
+    _.fold(handleLimited)(handleSuccess)
 
   private def handleSorting(sorting: Option[String])(hotels: Array[Hotel]): Array[Hotel] =
     sorting.map(_.toLowerCase).fold(hotels)({
@@ -15,9 +29,9 @@ object HotelsController {
   private val safeGetByCity = RateLimiter.wrapUnary(CSVBridge.getByCity, 5.seconds, 10)
   private val safeGetByRoom = RateLimiter.wrapUnary(CSVBridge.getByRoom, 10.seconds, 100)
 
-  def getByCity(city: String, sorting: Option[String]): Option[IO[Array[Hotel]]] =
-    safeGetByCity(city).map(_.map(handleSorting(sorting)))
+  def getByCity(city: String, sorting: Option[String]): IO[Response[IO]] =
+    handleReponse(safeGetByCity(city).map(_.map(handleSorting(sorting))))
 
-  def getByRoom(room: String, sorting: Option[String]): Option[IO[Array[Hotel]]] =
-    safeGetByRoom(room).map(_.map(handleSorting(sorting)))
+  def getByRoom(room: String, sorting: Option[String]): IO[Response[IO]] =
+    handleReponse(safeGetByRoom(room).map(_.map(handleSorting(sorting))))
 }
--- src/main/scala/com/agoda/ratelimiting/Router.scala
+++ src/main/scala/com/agoda/ratelimiting/Router.scala
@@ -1,35 +1,22 @@
 package com.agoda.ratelimiting
 
 import cats.effect.{IO, Sync}
-import cats.implicits._
-import org.http4s.{HttpRoutes, Response}
+import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
-import io.circe.syntax.EncoderOps
-import io.circe.generic.auto.exportEncoder
-import org.http4s.circe.CirceEntityEncoder._
-import com.agoda.ratelimiting.types.Hotel
 
 object Router {
 
   object PriceSortingParam extends OptionalQueryParamDecoderMatcher[String]("price-sorting")
-
-  private def handleSuccess: IO[Array[Hotel]] => IO[Response[IO]] =
-    _ >>= ((arr: Array[Hotel]) => Ok(arr.asJson))
-
-  private def handleLimited: IO[Response[IO]] = TooManyRequests("Too many requests")
-
-  private def handleReponse: Option[IO[Array[Hotel]]] => IO[Response[IO]] =
-    _.fold(handleLimited)(handleSuccess)
 
   def routes[F[_]: Sync]: HttpRoutes[IO] = {
 
     HttpRoutes.of[IO] {
 
       case GET -> Root / "city" / city :? PriceSortingParam(sorting) =>
-        handleReponse(HotelsController.getByCity(city, sorting))
+        HotelsController.getByCity(city, sorting)
 
       case GET -> Root / "room" / room :? PriceSortingParam(sorting) =>
-        handleReponse(HotelsController.getByRoom(room, sorting))
+        HotelsController.getByRoom(room, sorting)
 
     }
   }
