66091433760100b78a82692efe8921afa976edda
Bertrand31 <bertrandjun@gmail.com>
2020-01-13 15:32:32 +0100

    Add documentation to the bridge

--- src/main/scala/com/agoda/ratelimiting/Bridge.scala
+++ src/main/scala/com/agoda/ratelimiting/Bridge.scala
@@ -4,6 +4,12 @@ import scala.io.Source
 import cats.effect.IO
 import com.agoda.ratelimiting.types.Hotel
 
+/** The bridge is the abstraction through which the rest of the codebase has to go through to
+  * access our storage medium (be it a file, a database, etc.)
+  * Below is an implementation of the trait that reads from a CSV. If tomorrow we want to
+  * replace CSVs with a database, we will only need to write a new bridge that implements this
+  * trait, and the transition will be seamless.
+  */
 trait Bridge {
 
   def getByCity(city: String): IO[Array[Hotel]]
